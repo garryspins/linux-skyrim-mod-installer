ae13db0c8c31458746ba590affc2f7dc1ed36916
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2020-12-17 12:35:57 -0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if test -f /usr/local/bin/lsmi; then
+    echo "  Overriding/Updating Already Installed lsmi"
+fi
+
+if [ "$EUID" -ne 0 ]
+  then echo "  Run as root to install"
+  exit
+fi
+
+cp lsmi /usr/local/bin/
